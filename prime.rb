9606c37c1fa921c6e6314f619ee9fb2e30ddc794
Kimberly Aviles <01010110.v@gmail.com>
2018-11-13 10:25:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,15 @@ def prime?(integer)
 # make sure the number is not negative
   if integer <= 1
     return false
-  #elsif integer == 2 || integer == 3
-  #  return true
+# iterate over a range of integers to argument_integer
+# account for 'integer x 1 = integer' being detected (cuz that's every number ever) in the range
+# and remove it from the iteration
   else
     range = (2..integer - 1).to_a
+# grab the first iteration (if it it exists) that divides and leaves no remainder
     detection = range.detect { |num| integer % num == 0 }
+# if the iteration does not find any ranged integers that divide evenly into argument_integer
+# #prime? will return true (because it's a prime number! (primes cannot be divided evenly by anything other than 1))
     detection == nil ? true : false
   end
 end
