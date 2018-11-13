aba5efe535e2d7f09863258e8eb0701631629080
Kimberly Aviles <01010110.v@gmail.com>
2018-11-13 10:13:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 def prime?(integer)
-
+# make sure the number is not negative
   if integer <= 1
     return false
-  elsif integer == 2 || integer == 3
-    return true
+  #elsif integer == 2 || integer == 3
+  #  return true
   else
     range = (2..integer - 1).to_a
     detection = range.detect { |num| integer % num == 0 }
     detection == nil ? true : false
   end
-
-
 end
