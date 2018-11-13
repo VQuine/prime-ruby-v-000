0d847439169f19126703e7dc26b936799f2a1b80
Kimberly Aviles <01010110.v@gmail.com>
2018-11-13 09:12:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
   if integer > 1 && integer % 2 == 0
     return false
-  elsif integer > 1  
+  elsif integer > 1
     range = (1..integer).to_a
     detection = range.detect { |num| num * num == integer }
     detection == nil ? true : false
